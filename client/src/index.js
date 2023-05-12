eaa182da9ed8985e3acb1b5ec85bf25e079fa7b8
Aryan Garg <gargaryan82000@gmail.com>
2023-05-12 15:30:11 +0530

    Edited the Boostrap Packages

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,10 @@
+import $ from 'jquery';
 import App from './App';
 import React from 'react';
+import Popper from 'popper.js';
 import ReactDOM from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 /* Rendering Root that is to be attached with div#root */
 const root = ReactDOM.createRoot(document.getElementById('root'));
